e3ffe326126bef8a2e15275e340f3f4424c48018
karimliaghabala <agabala.93@gmail.com>
2025-12-05 12:03:16 +0100

    update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require("sequelize");
+require('dotenv').config()
 
-module.exports = new Sequelize("BigDataNews", "postgres", "3991001993", {
-  host: "localhost",
+module.exports = new Sequelize(process.env.POSTGRES_DATABASE, "postgres", process.env.POSTGRES_PASSWORD, {
+  host: process.env.POSTGRES_HOST,
   dialect: "postgres"
 });
+
+
+// postgres:3991001993@localhost:3000/
+
