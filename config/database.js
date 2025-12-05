a31c552db91fbb09c4c78284fdf2c6bbe4cd75eb
karimliaghabala <agabala.93@gmail.com>
2025-12-05 22:55:23 +0100

    update

--- config/database.js
+++ config/database.js
@@ -1,11 +1,22 @@
 const { Sequelize } = require("sequelize");
 require('dotenv').config()
 
-module.exports = new Sequelize(process.env.POSTGRES_DATABASE, "postgres", process.env.POSTGRES_PASSWORD, {
-  host: process.env.POSTGRES_HOST,
-  dialect: "postgres"
-});
-
+module.exports = new Sequelize(
+  process.env.POSTGRES_DATABASE,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
 // postgres:3991001993@localhost:3000/
 
