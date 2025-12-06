bdbc849b53e77c1fedd697e07dcbf40b65ccc0b5
karimliaghabala <agabala.93@gmail.com>
2025-12-06 02:02:00 +0100

    5

--- models/news.js
+++ models/news.js
@@ -1,21 +1,31 @@
-const Sequelize = require('sequelize')
+// const Sequelize = require('sequelize')
+// const db = require('../config/database.js')
 
-const db  = require('../config/database.js')
+// const News = db.define('news', {
+//     id: {
+//         type: Sequelize.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true
+//     },
+//     name: {
+//         type: Sequelize.STRING,
+//         allowNull: false
+//     },
+//     content: {
+//         type: Sequelize.TEXT,
+//         allowNull: false
+//     },
+//     createdAt: {
+//         type: Sequelize.DATE,
+//         defaultValue: Sequelize.NOW
+//     },
+//     updatedAt: {
+//         type: Sequelize.DATE,
+//         defaultValue: Sequelize.NOW
+//     }
+// }, {
+//     timestamps: true,
+//     tableName: 'news'
+// })
 
-
-const User = db.define('news',{
-    name:{
-        type:Sequelize.CHAR
-    },
-    content:{
-        type:Sequelize.TEXT
-    },
-    createdAt:{
-        type:Sequelize.TIME
-    },
-    updatedAt:{
-        type:Sequelize.TIME
-    },
-
-})
-module.exports=User
+// module.exports = News
--- router/userrouter.js
+++ router/userrouter.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../config/database.js");
 
-const User = require("../models/news.js");
-
-router.get("/", (req, res) => {
-  User.findAll()
-    .then((news) => res.json(news))
-    .catch((err) => console.log(err));
+// GET - Sadəcə SELECT FROM ilə
+router.get("/", async (req, res) => {
+  try {
+    const [news] = await db.query('SELECT * FROM "news"')
+    res.json(news)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Xəta baş verdi" })
+  }
 });
+
 // POST - Yeni məlumat əlavə et
-router.post("/", (req, res) => {
-  User.create(req.body)
-    .then((newUser) => {
-      res.status(201).json(newUser);
-      console.log("Yeni məlumat əlavə edildi");
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: "Məlumat əlavə olunmadı" });
-    });
+router.post("/", async (req, res) => {
+  try {
+    const { name, content } = req.body
+    await db.query(
+      'INSERT INTO "news" ("name", "content") VALUES ($1, $2)',
+      [name, content]
+    )
+    res.status(201).json({ message: "Məlumat əlavə edildi" })
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Xəta baş verdi" })
+  }
 });
 
 module.exports = router;
