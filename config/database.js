a2a3657a31760eeb1ad3305a3f309f8fcc10a6c5
karimliaghabala <agabala.93@gmail.com>
2025-12-06 01:17:11 +0100

    s

--- config/database.js
+++ config/database.js
@@ -1,6 +1,15 @@
 const { Sequelize } = require("sequelize");
 require('dotenv').config()
+import postgres from 'postgres';
+import { neon } from "@neondatabase/serverless";
 
+const sql = postgres(process.env.DATABASE_URL,  { ssl: 'verify-full' });
+
+export async function getData() {
+    const sql = neon(process.env.DATABASE_URL);
+    const data = await sql`SELECT * FROM posts;`;
+    return data;
+}
 module.exports = new Sequelize(
   process.env.PGDATABASE,
   process.env.PGUSER,
