44621742fd4e514efd085becff7233274f17aa0f
karimliaghabala <agabala.93@gmail.com>
2025-12-06 02:39:48 +0100

    x

--- index.js
+++ index.js
@@ -6,17 +6,23 @@ const bodyParser = require('body-parser')
 const app = express()
 const db = require('./config/database.js')
 
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 db.authenticate().then(()=>{
-    console.log("veri tabanina bagladi")
+    console.log("veri tabanina baglandi ✅")
 }).catch((err)=>{
-    console.log(err)
+    console.log("DB Xətası:", err)
 })
-app.use("/",require('./router/userrouter.js'))
 
-app.listen(3000,()=>{
-    console.log("3000 ishleyir")
-})
+app.use("/", require('./router/userrouter.js'))
+
+// Lokal üçün listen
+if (require.main === module) {
+  app.listen(3000, ()=>{
+    console.log("3000 portunda işləyir 🚀")
+  })
+}
+
+// ✅ VERCEL EXPORT - BU ÇOX VACIBDIR!
+module.exports = app
