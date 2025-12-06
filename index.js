38dccd2a8ee38d43f2b2d432f98acdb5f5a82d87
karimliaghabala <agabala.93@gmail.com>
2025-12-06 02:23:19 +0100

    d

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ db.authenticate().then(()=>{
 })
 app.use("/",require('./router/userrouter.js'))
 
-app.listen(process.env.PGHOST,()=>{
-    console.log("Online database")
+app.listen(process.env.DATABASE_URL,()=>{
+    console.log("3000 ishleyir")
 })
